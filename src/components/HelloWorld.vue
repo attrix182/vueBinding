bc32284ba5b76977312b33a16b88ba08306562fb
attrix <lucho.bernal@hotmail.com>
2021-06-01 01:14:19 -0300

    First commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,87 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container class="form">
+    <v-row class="text-center">
+      <v-col>
+        <v-text-field
+          v-model="name"
+          :rules="nameRules"
+          :counter="10"
+          label="Name"
+          required
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row class="text-center">
+      <v-col>
+        <v-text-field
+          v-model="lastname"
+          :rules="nameRules"
+          :counter="10"
+          label="Lastname"
+          required
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row class="text-center">
+      <v-col>
+        <v-text-field
+          v-model="image"
+          :counter="500"
+          label="URL Image"
+          required
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <br />
+    
+    <v-card class="mx-auto" max-width="400" v-if="image && name && lastname" >
+      <v-img
+        class="white--text align-end"
+        height="200px"
+        v-bind:src="image"
+      >
+   
+        <v-card-title></v-card-title>
+      </v-img>
+
+      <v-card-subtitle> <h1>Bienvenido</h1></v-card-subtitle>
+
+      <v-card-text class="text--primary">
+        <div> <h2>{{ name }} {{ lastname }}</h2></div>
+
+    
+      </v-card-text>
+
+      <v-card-actions>
+
+      </v-card-actions>
+    </v-card>
+
+  </v-container>
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    name: String,
+    lastname: String,
+    image:String,
+  },
+
+  data: () => ({
+    valid: false,
+    firstname: "",
+    lastname: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => v.length <= 10 || "Max 10 characters",
+    ],
+  }),
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -54,5 +99,9 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.form {
+  max-width: 300px;
 }
 </style>
